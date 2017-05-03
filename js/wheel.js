3125dc95db8bb3722e1f79293c3cefbf0eada5a1
Henrique Dias <heukirne@gmail.com>
2017-05-03 16:59:17 -0300

    add d3 timeseries lib

--- js/wheel.js
+++ js/wheel.js
@@ -6,8 +6,8 @@ var loadViz = function(){
 
 var loadData = function(){
 
-    var zipFile = "story_liwc_author_tiny.csv";
-    //var zipFile = "story_liwc_author.csv";
+    var zipFile = "story_liwc_author_tiny.csv"; // arquivo com 4K samples
+    //var zipFile = "story_liwc_author.csv"; // arquivo com 130K samples
     JSZipUtils.getBinaryContent('https://heukirne.github.io/wheel-of-life/js/' + zipFile + '.zip', (err, dataLoad) => {
         if(err) {
             throw err;
@@ -15,13 +15,26 @@ var loadData = function(){
 
         JSZip.loadAsync(dataLoad)
             .then(zip => {
+
+                  // descompacta arquivo csv
                   var content = zip.file(zipFile).async("string").then(content => {
                   var data = d3.csv.parseRows(content);
-                  data.splice(20, data.length)
-                  console.log(data);
-                  $('#title').text("Visualization");
 
+
+                  //remove linha de cabeçalho
+                  console.log(data[0]);
+                  data.splice(0,1);
+
+                  // deixa somente 20 linhas para depuração
+                  data.splice(200, data.length)
+
+                  //imprime os dados no console do browser
+                  console.log(data);
+
+                  // remove "Loading..." e imprime o gráfico
+                  $('#title').text("Visualization");
                   draw(data);
+
                 }
             )
         });
@@ -29,112 +42,20 @@ var loadData = function(){
 
 };
 
-// sentiment time-series mouseover
-// https://bl.ocks.org/BBischof/75ed4879d155c31412f58840480ae1d6
-
 var draw = function (data) {
 
-    var margin = {top: 20, right: 50, bottom: 30, left: 50},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+  var dateMap = new Map();
 
-    var bisecttime = d3.bisector(function(d) { return d.time; }).left,
-        formatValue = d3.format(",.2f"),
-        displayValue = function(d) { return formatValue(d); };
-        
-    var x = d3.scale.linear()
-        .range([0, width]);
+  data.forEach(d => {
+    d.date = new Date(d[67]);
+    d.n = +d[29]; // posemo
+    d.n3 = +d[30]; //negemo
+  });
 
-    var y = d3.scale.linear()
-        .range([height, 0]);
+  var chart = d3.timeseries()
+                .addSerie(data,{x:'date',y:'n',diff:'n3'},{color:"#333"})
+                .width(900)
 
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-    var line = d3.svg.line()
-        .x(function(d) { return x(d.time); })
-        .y(function(d) { return y(d.sentiment); });
-
-    var svg = d3.select("#d3chart").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    //d3.csv("https://bl.ocks.org/BBischof/raw/75ed4879d155c31412f58840480ae1d6/b9df15a40b5563841a724b95afae3449910cd818/data.csv", function(error, data) {
-    //  if (error) throw error;
-
-      data.splice(0,1);
-
-      data.forEach(function(d) {
-        d.time = +d[0];
-        d.sentiment = +d[5];
-      });
-
-      data.sort(function(a, b) {
-        return a.time - b.time;
-      });
-      
-      x.domain([data[0].time, data[data.length - 1].time]);
-      y.domain(d3.extent(data, function(d) { return d.sentiment; }));
-
-      svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
-
-      svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text("Sentiment");
-
-      svg.append("path")
-          .datum(data)
-          .attr("class", "line")
-          .attr("d", line);
-
-      var focus = svg.append("g")
-          .attr("class", "focus")
-          .style("display", "none");
-
-      focus.append("circle")
-          .attr("r", 4.5);
-
-      focus.append("text")
-          .attr("x", 9)
-          .attr("dy", ".35em");
-
-      svg.append("rect")
-          .attr("class", "overlay")
-          .attr("width", width)
-          .attr("height", height)
-          .on("mouseover", function() { focus.style("display", null); })
-          .on("mouseout", function() { focus.style("display", "none"); })
-          .on("mousemove", mousemove);
-
-      function mousemove() {
-        var x0 = x.invert(d3.mouse(this)[0]),
-            i = bisecttime(data, x0, 1),
-            d0 = data[i - 1],
-            d1 = data[i],
-            d = x0 - d0.time > d1.time - x0 ? d1 : d0;
-        focus.attr("transform", "translate(" + x(d.time) + "," + y(d.sentiment) + ")");
-        focus.select("text").text("Sentiment Value: " 
-                                  + displayValue(d.sentiment) 
-                                  + "\n Words: "
-    //                                                          + d.words
-                                 );
-      }
-    //});
+  chart('#d3chart')
 
 };
