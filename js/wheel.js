ed2ff9ed156b7dc3e2c95250b061ddeacde95230
Henrique Dias <heukirne@gmail.com>
2017-05-03 13:38:42 -0300

    update folders

--- js/wheel.js
+++
@@ -1,1 +0,0 @@
-var wheel = 1;
---
+++ wheel.js
@@ -0,0 +1,16 @@
+var zip = new JSZip();
+
+JSZipUtils.getBinaryContent('https://heukirne.github.io/wheel-of-life/data/corpus_liwc_mtx.csv.zip', (err, data) => {
+    if(err) {
+        throw err;
+    }
+
+    JSZip.loadAsync(data)
+        .then(zip => {
+            var content = zip.file("corpus_liwc_mtx.csv").async("string").then(content => {
+              var data = d3.csvParseRows(content);
+              console.log(data);
+            }
+        )
+    });
+});
